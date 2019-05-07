16686239eecde2c4cd7236fbdfe1c5a5b1b6218b
docfury2 <gregory.fennell1@gmail.com>
2019-05-07 11:46:35 -0400

    SSH and Paramiko

---
+++ paramikosshrsa/learntossh.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+##import paramiko so we can talk to ssh
+import paramiko
+import os
+
+## shortcut issuing comands to remote
+def commandissue(command_to_issue):
+    ssh_stdin, ssh_stdout, ssh_stderr = sshsession.exec_command(command_to_issue)
+    return ssh_stdout.read()
+
+sshsession = paramiko.SSHClient()
+
+##mykey is our private key
+mykey = paramiko.RSAKey.from_private_key_file("/home/student/.ssh/id_rsa")
+
+##if we never went to this SSH host, add the fingerprint to the known host file
+sshsession.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+##creds to connect
+sshsession.connect(hostname="10.10.2.3", username="bender", pkey=mykey)
+
+## a simple list of commands to issue across our connection
+our_commands = ["touch sshworked.txt", "touch create.txt", "touch file3.txt", "ls"]
+
+## cycle through our commands, and issue them on the far end
+for x in our_commands:
+    print(commandissue(x))
+
+print(commanissue(x).decode('utf-8'))
